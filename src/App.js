a9e3b579bb97c206352c870971eaf3f8fa7a9f89
unknown <neerajarao2898@gmail.com>
2023-07-03 17:48:08 +0530

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HeroPage from './components/HomePage/HeroPage'
+import Sec from './components/HomePage/Sec'
+import Third from './components/HomePage/Third'
+import Fourth from './components/HomePage/Fourth'
+import Fifth from './components/HomePage/Fifth'
 
 function App() {
   return (
     <div className="App">
       <h1>Hiclousia Frontend ,Hai</h1>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<>
+            {/* <SearchBar/>  */}
+            <HeroPage />
+            <Sec />
+            <Third />
+            <Fourth />
+            <Fifth />
+          </>} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/images/index.js
@@ -0,0 +1,13 @@
+import mainimg from "./mainimg.jpg"
+import second from "./second.jpg"
+import third from "./third.jpg"
+import asian1 from "./asian1.jpg"
+import asian2 from "./asian2.jpg"
+import map from "./map.jpg"
+import iitmrs from "./iitmrs.jpg"
+import iitmgate from "./iitmgate.jpg"
+import avatar from "./avatar.jpg"
+import Admin from "./Admin.svg"
+import logo from "./hiclousia_logo.jpg"
+
+export { mainimg ,Admin, third , second , asian1 , asian2 , map , logo, iitmrs , iitmgate ,avatar}
